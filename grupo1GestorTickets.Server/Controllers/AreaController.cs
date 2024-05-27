742a727318b8d3564f64e0c3a349108ee803063f
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2024-05-27 16:22:02 -0600

    Update AreaController.cs

--- grupo1GestorTickets.Server/Controllers/AreaController.cs
+++ grupo1GestorTickets.Server/Controllers/AreaController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using grupo1GestorTickets.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace grupo1GestorTickets.Server.Controllers
 {
-    public class AreaController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AreaController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public AreaController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+
+        // GET: api/area
+        [HttpGet("area")]
+        public async Task<ActionResult<IEnumerable<Area>>> GetAreas()
+        {
+            return await _context.Area.ToListAsync();
+        }
+        private bool AreaExists(int id)
+        {
+            return _context.Area.Any(e => e.Id == id);
         }
     }
 }
