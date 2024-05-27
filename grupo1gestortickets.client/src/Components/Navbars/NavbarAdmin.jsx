4e928c9cacc5fdf901525e9cdc3644dfef94da54
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2024-05-27 12:31:44 -0600

    navbs con bootstrap

--- grupo1gestortickets.client/src/Components/Navbars/NavbarAdmin.jsx
+++ grupo1gestortickets.client/src/Components/Navbars/NavbarAdmin.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NavbarAdmin = ({ onLogout }) => {
     return (
-        <nav>
-            <ul>
-                <li>Admin Dashboard</li>
-                <li><button onClick={onLogout}>Cerrar Sesion</button></li>
-            </ul>
-        </nav>
+        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand as={Link} to="/admin">Administrador</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to="/control-usuarios">Control de Usuarios</Nav.Link>
+                        <Nav.Link as={Link} to="/registrar-usuarios">Registrar Usuarios</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Button variant="outline-light" onClick={onLogout}>Cerrar Sesion</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
 export default NavbarAdmin;
+
+
+
 
