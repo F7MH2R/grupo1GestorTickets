b4a9e5cb9b0dc34f58782be3aadebf243a6b7119
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2024-05-27 16:21:16 -0600

    controllers

---
+++ grupo1GestorTickets.Server/Controllers/AreaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace grupo1GestorTickets.Server.Controllers
+{
+    public class AreaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
