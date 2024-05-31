2c05472f1c59fc340933a6289049b89477fb67a2
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2024-05-30 20:33:53 -0600

    Cambios iniciales, api de inicio admin

---
+++ grupo1GestorTickets.Server/Controllers/InicioAdminController.cs
@@ -0,0 +1,32 @@
+﻿using grupo1GestorTickets.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace grupo1GestorTickets.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InicioAdminController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InicioAdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTickets()
+        {
+            var tickets = await _context.Ticket.Include(t => t.Estado).ToListAsync();
+            var ticketsAbiertos = await _context.Ticket.Include(t => t.Estado).Where(ticket => ticket.Estado.Nombre.Equals("Abierto")).CountAsync();
+            var ticketsEnProceso = await _context.Ticket.Include(t => t.Estado).Where(ticket => ticket.Estado.Nombre.Equals("En Proceso")).CountAsync();
+            return Ok(new {
+                tickets,
+                ticketsAbiertos,
+                ticketsEnProceso
+            });
+        }
+    }
+}
