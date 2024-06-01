94477fdcbe12164aef00f5d352e13473280d91a8
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2024-06-01 17:37:48 -0600

    Obtener estadisticas para pagina de admin

--- grupo1GestorTickets.Server/Controllers/InicioAdminController.cs
+++ grupo1GestorTickets.Server/Controllers/InicioAdminController.cs
@@ -1,5 +1,4 @@
 ﻿using grupo1GestorTickets.Server.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,10 +19,43 @@ namespace grupo1GestorTickets.Server.Controllers
         public async Task<IActionResult> GetTickets()
         {
             var tickets = await _context.Tickets.ToListAsync();
+            var cantidadAbiertos = await obtenerTicketsxEstado("abierto");
+            var cantidadCerrados = await obtenerTicketsxEstado("cerrado");
+            var cantidadEnProceso = await obtenerTicketsxEstado("cerrado");
+            var cantidadSinAsignar = await ticketsSinAsignar();
             //
             return Ok(new {
-                tickets
+                abiertos = cantidadAbiertos.Count,
+                cerrados = cantidadCerrados.Count,
+                enProceso = cantidadEnProceso.Count,
+                sinAsignar = cantidadSinAsignar.Count,
+                tickets,
             });
+        }
+
+
+
+        private Task<List<Ticket>> obtenerTicketsxEstado(string estado)
+        {
+            return (from
+                        t in _context.Tickets
+                    join
+                        e in _context.Estados
+                    on 
+                        t.IdEstado equals e.Id
+                    where 
+                        (e.Estado1.ToLower() == estado)
+                    select 
+                        t
+                    ).ToListAsync();
+        }
+
+        private Task<List<Ticket>> ticketsSinAsignar()
+        {
+            return (from t in _context.Tickets
+                     where t.IdUsuarioAsignado == null
+                     select t
+                ).ToListAsync();
         }
     }
 }
