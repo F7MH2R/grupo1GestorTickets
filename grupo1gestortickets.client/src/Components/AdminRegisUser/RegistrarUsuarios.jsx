03950bebbac36ee71dad3835380babd982fd6546
jaglorena <loremedi0103@gmail.com>
2024-06-05 13:39:43 -0600

    aplicando estilos a los registros

--- grupo1gestortickets.client/src/Components/AdminRegisUser/RegistrarUsuarios.jsx
+++ grupo1gestortickets.client/src/Components/AdminRegisUser/RegistrarUsuarios.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Container, Row, Col, Card } from 'react-bootstrap';
 import withLoader from "../Load/withLoader ";
 import { Link, useNavigate } from 'react-router-dom';
+import "./RegistrarUsuarios.css";
 
 const RegistrarUsuarios = () => {
     const navigate = useNavigate();
@@ -11,14 +12,14 @@ const RegistrarUsuarios = () => {
     };
 
     return (
-        <Container className="mt-5 pt-5">
+        <Container className="container-registrar mt-5 pt-5">
             <h1 className="text-center mb-4">Registrar</h1>
             <Row className="justify-content-center">
                 <Col xs={12} md={6} lg={4} className="mb-4">
                     <Card className="text-center">
                         <Card.Body>
                             <Card.Img variant="top" src="https://i.ibb.co/qs6XMdg/administrador.jpg" alt="Registrar Admin" />
-                            <Button style={{ color: "black", background: "#FDDD00" }} variant="warning" className="mt-3" as={Link} to="/registrar-administrador">Registrar Admin</Button>
+                            <Button  variant="warning" className="btn-usuarios mt-3" as={Link} to="/registrar-administrador">Registrar Admin</Button>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -26,7 +27,7 @@ const RegistrarUsuarios = () => {
                     <Card className="text-center">
                         <Card.Body>
                             <Card.Img variant="top" src="https://i.ibb.co/N9hmWTG/empleado.jpg" alt="Registrar Empleado" />
-                            <Button variant="warning" className="mt-3" as={Link} to="/registrar-empleado">Registrar Empleado</Button>
+                            <Button variant="warning" className="btn-usuarios mt-3" as={Link} to="/registrar-empleado">Registrar Empleado</Button>
                         </Card.Body>
                     </Card>
                 </Col>
